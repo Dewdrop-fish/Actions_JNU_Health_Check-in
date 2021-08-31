744b37e88a287f6929a7e4bbed9ae6a01f812e83
Dewdrop-fish <89769398+Dewdrop-fish@users.noreply.github.com>
2021-08-31 19:14:07 +0800

    Update notice.go

--- notice.go
+++ notice.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
 func PostNotice(msg string, sckey string) {
 
+	var title string
+	if msg == "重复提交问卷" {
+		title = "健康状态申报重复"
+	} else if msg == "插入问卷数据成功" {
+		title = "您已完成今天的健康状态申报"
+	}
+
 	data := url.Values{}
-	data.Add("title", "check-in_results")
+	data.Add("title", title)
 	data.Add("desp", msg)
 
 	resp, err := http.PostForm("https://sctapi.ftqq.com/"+sckey+".send", data)
 	if err != nil {
 		println(err)
 	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	// defer
+	resp.Body.Close()
+	// ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
 
 }
