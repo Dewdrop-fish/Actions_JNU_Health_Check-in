10c867455d9633f42b9d3fde7c98b11705585c9a
Dewdrop-fish <89769398+Dewdrop-fish@users.noreply.github.com>
2021-08-31 19:09:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,19 +6,6 @@ import (
 )
 
 func main() {
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
 	sckey := os.Getenv("SCKEY")
-
-	password = Encrypt(password)
-	jnuID := Login(username, password)
-	firstID := List(jnuID)
-	mainTable, secondTable := Review(firstID, jnuID)
-	_, msg := Checkin(jnuID, mainTable, secondTable)
-
-	fmt.Println(msg)
-	if len(sckey) > 1 {
-		PostNotice(msg, sckey)
+	fmt.Println(sckey)
 	}
-
-}
